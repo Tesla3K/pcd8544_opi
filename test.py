122eaab37cef24d062b25da0fcb847ad3f12ea12
Hojjat Abedie <hojjatabedie@gmail.com>
2018-12-15 22:19:17 +0330

    fix import mistacke

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 import cppimport
-LCDInit = cppimport.imp("LCDInit")
-LCDclear = cppimport.imp("LCDclear")
-LCDdrawChar = cppimport.imp("LCDdrawChar") #This will pause for a moment to compile the module
-LCDdisplay = cppimport.imp("LCDdisplay")
+nokia = cppimport.imp("nokia")
 
-LCDInit(14, 12, 5, 10, 4, 50)
-LCDclear()
-LCDdrawChar(1, 0, 'A', 0, 0)
-LCDdisplay()
+nokia.LCDInit(14, 12, 5, 10, 4, 50)
+nokia.LCDclear()
+nokia.LCDdrawChar(1, 0, 'A', 0, 0)
+nokia.LCDdisplay()
